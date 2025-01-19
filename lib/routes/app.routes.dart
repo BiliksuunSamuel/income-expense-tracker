8d722f513e8483cd312bd2b2af1ad5b05f05ba90
BiliksuunSamuel <samuel.biliksuun@hubtel.com>
2025-01-19 16:12:27 +0000

    removed unused files and variables

--- lib/routes/app.routes.dart
+++ lib/routes/app.routes.dart
@@ -5,16 +5,6 @@ import '../screens/home/home.screen.dart';
 
 class AppRoutes {
   static const String home = '/';
-  static const constituencies = "/constituencies";
-  static const pollingAgents = "/polling-agents";
-  static const profile = "/profile";
-  static const users = "/users";
-  static const pollingStations = "/polling-stations";
-  static const userCreate = "/user-create";
-  static const constituencyCreate = "/constituency-create";
-  static const pollingStationCreate = "/polling-station-create";
-  static const electionCreate = "/election-create";
-  static const electionDetails = "/election-details";
   static const welcomeScreen = "/welcome";
   static List<GetPage> routes = [
     GetPage(name: home, page: () => const HomeScreen()),
