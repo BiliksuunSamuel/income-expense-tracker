70ed0db514e240e2cc25c45eb9e63f3c60e921ae
BiliksuunSamuel <samuel.biliksuun@hubtel.com>
2025-08-31 23:04:21 +0000

    Add post-clone script for Xcode Cloud Flutter setup

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-set -e
+#!/bin/zsh
 
-echo "🔁 Running post-clone script"
-flutter --version
-dart --version
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install CocoaPods (if not already installed)
+sudo gem install cocoapods
+
+# Get Flutter dependencies
+flutter pub get
+
+# Get iOS dependencies
+cd ios
+pod install
+cd ..
